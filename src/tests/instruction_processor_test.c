7256ea26ba745ac01cda1bef93dd66d1f3955f2c
Sihan Tao <sihan.tao20@imperial.ac.uk>
2021-05-27 16:01:23 -0700

    Implement the find type function for instructions and write some tests for it. But the tests fail

--- src/tests/instruction_processor_test.c
+++ src/tests/instruction_processor_test.c
@@ -66,11 +66,88 @@ int main(void)
     {
 
     }
-    end_test;
+    end_test();
 
     add_test("test branch in union");
     {
 
+    }
+    end_test();
+
+    add_test("Find the type of instructions:");
+    {
+        /* The example from P5 in the spec:
+         * mov r1, #1       data processing
+         * b foo            branch
+         * mov r2, #2       data processing
+         * foo: mov r3, #3  data processing
+         */
+        instruction_t instructions[] = {
+            {0, {0xe3a01001}}, {0, {0xea000000}},
+            {0, {0xe3a02002}}, {0, {0xe3a03003}}
+        };
+
+        instruction_t *p = instructions;
+        find_type(p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, DATA_PROCESS, "mov r1, #1 == data processing");
+
+        find_type(++p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, BRANCH, "b foo == branch");
+
+        find_type(++p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, DATA_PROCESS, "mov r2, #2 == data processing");
+
+        find_type(p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, DATA_PROCESS, "foo: mov r3, #3 == data processing");
+
+        /* add01
+         * mov r1,#1
+         * add r2,r1,#2 
+         */
+        instruction_t add01[] = {{0, {0xe3a01001}}, {0, {0xe2812002}}};
+        find_type(&add01[0]);
+	    print_bit(add01[0].u.i);
+        test_int_v(add01[0].tag, DATA_PROCESS, "mov r1,#1 == DATA_PROCESS");
+        
+        find_type(&add01[1]);
+	    print_bit(add01[1].u.i);
+        test_int_v(add01[1].tag, DATA_PROCESS, "add r2,r1,#2 == DATA_PROCESS");
+
+        instruction_t mul = {0, {0xe0020091}}; //9100 02e0 mul r2,r1,r0
+        find_type(&mul);
+	    print_bit(mul.u.i);
+        test_int_v(mul.tag, MUL, "mul r2,r1,r0 == multiply");
+
+        instruction_t bne = {0, {0x1afffffa}};// faff ff1a bne loop
+        find_type(&bne);
+	    print_bit(bne.u.i);
+        test_int_v(bne.tag, BRANCH, "bne == BRANCH");
+
+        /*
+         * ldr r0,=0x02 0200 a0e3
+         * ldr r2,[r0]  0020 90e5
+         * cmp r2,r0    0000 52e1
+         */
+        instruction_t ldr01[] = {
+            {0, {0xe3a00002}}, {0, {0xe5902000}}, {0, {0xe1520000}}
+        };
+
+        p = ldr01;
+        find_type(p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, TRANS, "ldr r0,=0x02 == TRANS");
+        p++;
+        find_type(p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, TRANS, "ldr r2,[r0] == TRANS");
+        p++;
+        find_type(p);
+	    print_bit(p->u.i);
+        test_int_v(p->tag, TRANS, "cmp r2,r0 == TRANS");
     }
     end_test();
     return 0;
