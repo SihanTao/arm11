5e7954dacc1d6efe99e5baaab6edef5b8d80362f
tony <tony_t_liu@outlook.com>
2021-05-28 22:28:20 +0800

    add makefile to each dir

--- src/emulator/main/emulate.h
+++ src/emulator/main/emulate.h
@@ -8,6 +8,6 @@ typedef struct pipeline_state_struct {
 
 typedef pipeline_state_struct * Pipeline;
 
-# define FLASH_CYCLE swap(&next, &current); states->pc++;
+# define FLASH_CYCLE temp = current; current = next; next = temp; states->pc++;
 
 #endif //EMULATE
