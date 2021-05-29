e1045f657022df55ed3fa70c03d4ea4efa8bb122
cl2420 <lcx@LAPTOP-QD7DAL3K.localdomain>
2021-05-30 01:30:59 +0800

    add_opcode_in_data_processing

--- src/emulator/execute/execute.c
+++ src/emulator/execute/execute.c
@@ -30,30 +30,70 @@ bool execute(instruction_t* decode, ArmState armstate)
     }
 }
 
-
-void execute_MUL(instruction_t* decoded, ArmState armstate)
+void execute_DP(instruction_t* decode, ArmState armstate)
 {
     uint32_t result;
 
-    uint32_t Rm = bitfield_to_uint32(armstate->reg[decoded->u.mul.Rm]);
-    uint32_t Rs = bitfield_to_uint32(armstate->reg[decoded->u.mul.Rs]);
-    result = Rm * Rs;
-    
-    // the accumulate bit is set
-    if (decoded->u.mul.A)
+    uint32_t Rn = bitfield_to_uint32(armstate->reg[decode->u.data_process.Rn]);
+    uint32_t operand2 = bitfield_to_uint32(armstate->reg[decode->u.data_process.operand2]);
+
+    // Type of Opcode
+    if (decode->u.data_process.OpCode)
     {
-        uint32_t Rn = bitfield_to_uint32(armstate->reg[decoded->u.mul.Rn]);
-        result += Rn;
+        switch (decode->u.data_process.OpCode)
+        {
+            case AND: return (Rn && operand2);
+            case EOR: return (Rn ^ operand2);
+            case SUB: return (Rn - operand2);
+            case RSB: return (operand2 - Rn);
+            case ADD: return (Rn + operand2);
+            case TST: (Rn && operand2);
+            case TEQ: (Rn ^ operand2);
+            case CMP: (Rn - operand2);
+            case ORR: return (Rn || operand2);
+            case MOV: return (operand2);
+            default:
+                break;
+        }
     }
-    // Save the result
-    armstate->reg[decoded->u.mul.Rd] = uint32_to_bitfield(result);
     
+
     // If the S bit is set, we need to update the CPSR
-    if (decoded->u.mul.S)
+    if (decode->u.mul.S)
     {
         armstate->flagN = get_k_bit(result, 31);
         armstate->flagZ = (!result) ? 1 : 0;
     }
+}
+
+void execute_MUL(instruction_t* decode, ArmState armstate)
+{
+    uint32_t result;
+
+    uint32_t Rm = bitfield_to_uint32(armstate->reg[decode->u.mul.Rm]);
+    uint32_t Rs = bitfield_to_uint32(armstate->reg[decode->u.mul.Rs]);
+    result = Rm * Rs;
+    
+    // the accumulate bit is set
+    if (decode->u.mul.A)
+    {
+        uint32_t Rn = bitfield_to_uint32(armstate->reg[decode->u.mul.Rn]);
+        result += Rn;
+    }
+    // Save the result
+    armstate->reg[decode->u.mul.Rd] = uint32_to_bitfield(result);
+    
+    // If the S bit is set, we need to update the CPSR
+    if (decode->u.mul.S)
+    {
+        armstate->flagN = get_k_bit(result, 31);
+        armstate->flagZ = (!result) ? 1 : 0;
+    }
+}
+
+void execute_BRANCH(instruction_t* decode, ArmState armstate)
+{
+    
 }
 
 bool test_instruction_cond(instruction_t* instruction, ArmState armstate)
