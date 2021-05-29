48182e606cf929063529dda1741d82c466e89f8d
Sihan Tao <sihan.tao20@imperial.ac.uk>
2021-05-29 09:05:06 -0700

    Modify decode.c to use tools.h

--- src/emulator/decode/decode.c
+++ src/emulator/decode/decode.c
@@ -1,29 +1,31 @@
 #include <stdlib.h>
 #include "../utils/types_and_macros.h"
+#include "../utils/tools.h"
 
-// this will not compile so commented
-// instruction_t decode (bitfield fetched)
-// {
-//     return NULL;
-// }
+instruction_t decode (bitfield fetched)
+{
+    instruction_t decode = init_instruction(fetched);
+    find_type(&decode);
+    return decode;
+}
 
-// void find_type(instruction_t* instruction)
-// {
-//     uint32_t n = instruction->u.i;
-//     if (get_k_bit(n, 27))
-//     {
-//         instruction->tag = BRANCH;
-//     } else if (get_k_bit(n, 26))
-//     {
-//         instruction->tag = TRANS;
-//     } else if (get_k_bit(n, 25))
-//     {
-//         instruction->tag = DATA_PROCESS; // the case with op2 an immediate value
-//     } else if (get_k_bit(n, 4) && get_k_bit(n, 7))
-//     {
-//         instruction->tag = MUL;
-//     } else
-//     {
-//         instruction->tag = DATA_PROCESS;
-//     }
-// }
+void find_type(instruction_t* instruction)
+{
+    uint32_t n = instruction->u.i;
+    if (get_k_bit(n, 27))
+    {
+        instruction->tag = BRANCH;
+    } else if (get_k_bit(n, 26))
+    {
+        instruction->tag = TRANS;
+    } else if (get_k_bit(n, 25))
+    {
+        instruction->tag = DATA_PROCESS; // the case with op2 an immediate value
+    } else if (get_k_bit(n, 4) && get_k_bit(n, 7))
+    {
+        instruction->tag = MUL;
+    } else
+    {
+        instruction->tag = DATA_PROCESS;
+    }
+}
