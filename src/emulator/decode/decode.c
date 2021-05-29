b36e3d0d058701b72317826362e8f8c0ef6ec3c7
Sihan Tao <sihan.tao20@imperial.ac.uk>
2021-05-29 02:47:45 -0700

    Improve the type of decode.

--- src/emulator/decode/decode.c
+++ src/emulator/decode/decode.c
@@ -8,16 +8,16 @@ instruction_t decode (bitfield fetched)
 void find_type(instruction_t* instruction)
 {
     uint32_t n = instruction->u.i;
-    if (get_k_bit(n, 27) == 1)
+    if (get_k_bit(n, 27))
     {
         instruction->tag = BRANCH;
-    } else if (get_k_bit(n, 26) == 1)
+    } else if (get_k_bit(n, 26))
     {
         instruction->tag = TRANS;
-    } else if (get_k_bit(n, 25) == 1)
+    } else if (get_k_bit(n, 25))
     {
         instruction->tag = DATA_PROCESS; // the case with op2 an immediate value
-    } else if (get_k_bit(n, 4) == 1 && get_k_bit(n, 7) == 1)
+    } else if (get_k_bit(n, 4) && get_k_bit(n, 7))
     {
         instruction->tag = MUL;
     } else 
