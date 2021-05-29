483dc590704d905d9027ddf7783c2a761cbb65e7
Sihan Tao <sihan.tao20@imperial.ac.uk>
2021-05-29 02:24:26 -0700

    Implement the interface for execute

---
+++ src/emulator/execute/execute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "types_and_macros.h"
+#include "execute.h"
+
+bool execute(instruction_t* decode, ArmState armstate)
+{
+    if ()
+    {
+        /* code */
+    }
+    
+}
--- src/emulator/execute/execute.h
+++ src/emulator/execute/execute.h
@@ -2,7 +2,11 @@
 #define EXECUTE
 
 // execute will not receive NULL value;
-extern bool execute(bitfield fetched, ArmState memory);
+extern bool execute(instruction_t* decode, ArmState armstate);
+void execute_DP(instruction_t* decode, Armstate armstate);
+void execute_MUL(instruction_t* decode, Armstate armstate);
+void execute_SDT(instruction_t* decode, Armstate armstate);
+void execute_BRANCH(instruction_t* decode, Armstate armstate);
 
 #define EXIT true;
 #define CONTINUE false;
