261b299d2e62e3e07930fe94c34709dc0f3bdd01
tony <tony_t_liu@outlook.com>
2021-06-05 00:34:29 +0800

    commented decode

--- src/emulator/decode/decode.c
+++ src/emulator/decode/decode.c
@@ -3,38 +3,45 @@
 #include "../utils/tools.h"
 #include "decode.h"
 
-instruction_t decode (bitfield fetched)
+instruction_t decode(bitfield fetched)
 {
-    instruction_t decode = init_instruction(fetched);
-    find_type(&decode);
-    return decode;
+  instruction_t decode = init_instruction(fetched);
+  find_type(&decode);
+  return decode;
 }
 
-void find_type(instruction_t* instruction)
+void find_type(instruction_t *instruction)
 {
-    uint32_t n = instruction->u.i;
-    if (get_bit(n, 27))
-    {
-        instruction->tag = BRANCH;
-    } else if (get_bit(n, 26))
-    {
-        instruction->tag = TRANS;
-    } else if (get_bit(n, 25))
-    {
-        instruction->tag = DATA_PROCESS; // the case with op2 an immediate value
-    } else if (get_bit(n, 4) && get_bit(n, 7))
-    {
-        instruction->tag = MUL;
-    } else
-    {
-        instruction->tag = DATA_PROCESS;
-    }
+  // TODO : 1. handle ZERO instruction
+  //        2. default case : tag undefined
+
+  uint32_t n = instruction->word.i;
+  if (get_bit(n, 27))
+  {
+    instruction->tag = BRANCH;
+  }
+  else if (get_bit(n, 26))
+  {
+    instruction->tag = TRANS;
+  }
+  else if (get_bit(n, 25))
+  {
+    instruction->tag = DATA_PROCESS; // the case with op2 an immediate value
+  }
+  else if (get_bit(n, 4) && get_bit(n, 7))
+  {
+    instruction->tag = MUL;
+  }
+  else
+  {
+    instruction->tag = DATA_PROCESS;
+  }
 }
 
 instruction_t init_instruction(bitfield fetched)
 {
   instruction_t init;
   init.tag = UNDEFINED;
-  init.u.bf = fetched;
+  init.word.bf = fetched;
   return init;
 }
--- src/emulator/decode/decode.h
+++ src/emulator/decode/decode.h
@@ -1,11 +1,18 @@
 #ifndef DECODE
 #define DECODE
 
-/* decode will initialize an instruction
+/*
+ * decode will initialize an instruction
  * then return an instruction_t with tag
  */
 instruction_t decode (bitfield fetched);
+
 void find_type(instruction_t* instruction);
+
+/*
+ * initialize an instruction to UNDEFINED
+ * category.
+ */
 instruction_t init_instruction(bitfield fetched);
 
 #endif //DECODE
