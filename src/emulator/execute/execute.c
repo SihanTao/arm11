dc6b70255c812b1e162010865ef72ed7ce92d43a
tony <tony_t_liu@outlook.com>
2021-06-10 03:12:29 +0800

    Tony: rewrote single data transfer

--- src/emulator/execute/execute.c
+++ src/emulator/execute/execute.c
@@ -5,202 +5,174 @@
 
 #include "execute.h"
 
-#include "../execute/execute_SDT.h"
 #include "../execute/execute_helper.h"
 #include "../utils/tools.h"
 
-exit_type execute (instruction_t decoded, ArmState arm_state)
+static uint32_t dp_carried_result(proc_type opcode, uint32_t Rn,
+                                  uint32_t operand2, bool *newFlagC);
+
+exit_type execute(instruction_t decoded, ArmState arm_state)
 {
-  if (test_instruction_cond (decoded, arm_state))
+  if (test_instruction_cond(decoded, arm_state))
+  {
+    switch (decoded.tag)
     {
-      switch (decoded.tag)
-        {
-        case DATA_PROCESS:
-          execute_DP (decoded.word.proc, arm_state);
-          return CONTINUE;
-        case MUL:
-          execute_MUL (decoded.word.mul, arm_state);
-          return CONTINUE;
-        case TRANS:
-          execute_TRANS (decoded.word.trans, arm_state);
-          return CONTINUE;
-        case BRANCH:
-          execute_BRANCH (decoded.word.branch, arm_state);
-          return CONTINUE;
-        case ZERO:
-          return EXIT;
-        default:
-          return ERROR;
-        }
+    case DATA_PROCESS:
+      execute_DP(decoded.word.proc, arm_state);
+      return CONTINUE;
+    case MUL:
+      execute_MUL(decoded.word.mul, arm_state);
+      return CONTINUE;
+    case TRANS:
+      execute_TRANS(decoded.word.trans, arm_state);
+      return CONTINUE;
+    case BRANCH:
+      execute_BRANCH(decoded.word.branch, arm_state);
+      return CONTINUE;
+    case ZERO:
+      return EXIT;
+    default:
+      return ERROR;
     }
+  }
+
+  return CONTINUE;
 }
 
-void execute_DP (proc_t instruction, ArmState arm_state)
+void execute_DP(proc_t instruction, ArmState arm_state)
 {
   bitfield *reg = arm_state->reg;
-  uint32_t Rn = to_int (reg[instruction.Rn]);
+  uint32_t  Rn  = to_int(reg[instruction.Rn]);
+  uint32_t  operand2;
+  bool      newFlagC;
 
-  value_carry_t value_carry
-      = reg_imm_handle (reg, instruction.operand2, instruction.is_imm);
-  uint32_t operand2 = value_carry.value;
-  bool newFlagC = value_carry.carry;
-  // set to shift operation carry out by default
-  uint32_t result = compute_DP_result_and_carry (instruction.opcode, Rn,
-                                                 operand2, &newFlagC);
+  // compute operand2, carry out is set to shift barrel carry out
+  reg_imm_handle(reg, instruction.operand2, instruction.is_imm, &operand2,
+                 &newFlagC);
+
+  uint32_t result
+      = dp_carried_result(instruction.opcode, Rn, operand2, &newFlagC);
 
   // don't update the result if opcode is TST, TEQ or CMP
   if (instruction.opcode != TST && instruction.opcode != TEQ
       && instruction.opcode != CMP)
-    {
-      reg[instruction.Rd] = to_bf (result);
-    }
+  {
+    reg[instruction.Rd] = to_bf(result);
+  }
 
+  // if set_cond bit is set, change the CPRS flags
   if (instruction.set_cond)
-    {
-      arm_state->neg = get_bit (result, 31);
-      arm_state->zero = result == 0;
-      arm_state->carry = newFlagC;
-    }
+  {
+    arm_state->neg   = get_bit(result, 31);
+    arm_state->zero  = result == 0;
+    arm_state->carry = newFlagC;
+  }
 }
 
-uint32_t compute_DP_result_and_carry (proc_type opcode, uint32_t Rn,
-                                      uint32_t operand2, bool *newFlagC)
+static uint32_t dp_carried_result(proc_type opcode, uint32_t Rn,
+                                  uint32_t operand2, bool *newFlagC)
 {
   switch (opcode)
-    {
-    // for logical operations, C is set to the carry out from any shift
-    // operation i.e. by no change
-    case AND:
-      return Rn && operand2;
-    case EOR:
-      return Rn ^ operand2;
-    case ORR:
-      return Rn || operand2;
-    case TST:
-      return Rn && operand2;
-    case TEQ:
-      return Rn ^ operand2;
-    case MOV:
-      return operand2;
-    // In arithmetic operations (add, sub, rsb and cmp) the C bit will be set
-    // to the carry out of the bit 31 of the ALU
+  {
+  // for logical operations, C is set to the carry out from any shift
+  // operation i.e. by no change
+  case AND:
+    return Rn && operand2;
+  case EOR:
+    return Rn ^ operand2;
+  case ORR:
+    return Rn || operand2;
+  case TST:
+    return Rn && operand2;
+  case TEQ:
+    return Rn ^ operand2;
+  case MOV:
+    return operand2;
+  // In arithmetic operations (add, sub, rsb and cmp) the C bit will be set
+  // to the carry out of the bit 31 of the ALU
 
-    // TODO!!!!!!
-    // there will be problems, that I am not sure how unsigned or signed add
-    // and mul performed.
-    case SUB:
-      *newFlagC = get_bit (Rn, 31) && get_bit (Rn, 31); // might be a bug here
-      return Rn - operand2;
-    case RSB:
-      *newFlagC = 1;
-      return operand2 - Rn;
-    case ADD:
-      *newFlagC = 1;
-      return Rn + operand2;
-    case CMP:
-      *newFlagC = 1;
-      return Rn - operand2; // result not written
-    default:
-      break;
-    }
+  // TODO!!!!!!
+  // there will be problems, that I am not sure how unsigned or signed add
+  // and mul performed.
+  case SUB:
+    *newFlagC = get_bit(Rn, 31) && get_bit(Rn, 31); // might be a bug here
+    return Rn - operand2;
+  case RSB:
+    *newFlagC = 1;
+    return operand2 - Rn;
+  case ADD:
+    *newFlagC = 1;
+    return Rn + operand2;
+  case CMP:
+    *newFlagC = 1;
+    return Rn - operand2; // result not written
+  default:
+    break;
+  }
 }
 
-void execute_TRANS (trans_t instruction, ArmState arm_state)
+void execute_TRANS(trans_t instruction, ArmState arm_state)
 {
-  uint32_t result;
   bitfield *reg = arm_state->reg;
+  uint32_t  Rn  = to_int(reg[instruction.Rn]);
+  uint32_t  offset;
 
-  uint32_t Rn = to_int (reg[instruction.Rn]);
-  uint32_t offset
-      = reg_imm_handle (reg, instruction.offset, instruction.is_imm);
+  reg_imm_handle(reg, instruction.offset, instruction.is_imm, &offset, NULL);
 
   // if is_up is set then offset is added to Rn. Otherwise the offset is
   // subtracted from Rn.
   Rn = (instruction.is_up) ? Rn + offset : Rn - offset;
 
-  // If is_load is set, the word is loaded from memory, otherwise the word is
-  // stored into memory.
-  if (instruction.is_load)
-    {
-      result = Rn;
-    }
-  else
-    {
-      reg[instruction.Rd] = to_bf (Rn);
-    }
+  // For the purposes of this exercise, pre- indexing will not change the value
+  // of the base register, however post-indexing will change the contents of
+  // the base register by the offset.
+  if (!instruction.is_pre) // pre indexing
+  {
+    reg[instruction.Rn] = to_bf(Rn);
+  }
 
-  if (instruction.is_pre) // pre-indexing, the offset is added/subtracted to
-                          // the base register before transferring the data.
-    {
-      uint32_t newRn = (instruction.is_up) ? Rn + offset : Rn - offset;
-
-      switch (instruction.is_pre)
-        {
-        case LOAD:
-          result = to_int (reg[instruction.Rn]) + to_int (reg[offset]);
-          break;
-
-        case STORE:
-          reg[instruction.Rd] = to_bf (newRn);
-          break;
-
-        default:
-          break;
-        }
-      Rn = (instruction.is_up) ? Rn + offset : Rn - offset;
-    }
-  else //(post-indexing, the offset is added/subtracted to the base register
-       // after transferring.
-    {
-      switch (instruction.is_pre)
-        {
-        case LOAD:
-          result = Rn;
-          break;
-
-        case STORE:
-          reg[instruction.Rd] = to_bf (Rn);
-          break;
-
-        default:
-          break;
-        }
-      Rn = (instruction.is_up) ? Rn + offset : Rn - offset;
-    }
+  if (instruction.is_load) // load from memory
+  {
+    reg[instruction.Rd] = load(Rn, arm_state->memory);
+  }
+  else // store to memory
+  {
+    // poke(reg[instruction.Rd], Rn, arm_state->memory);
+  }
 }
 
-void execute_MUL (mul_t instruction, ArmState arm_state)
+void execute_MUL(mul_t instruction, ArmState arm_state)
 {
   // pre: PC is not used as operand or destination register
   //      Rd will not be the same as Rm
-  bitfield *reg = arm_state->reg;
-  uint32_t Rm = to_int (reg[instruction.Rm]);
-  uint32_t Rs = to_int (reg[instruction.Rs]);
-  uint32_t Rn = to_int (reg[instruction.Rn]);
-  uint32_t result = Rm * Rs;
+  bitfield *reg    = arm_state->reg;
+  uint32_t  Rm     = to_int(reg[instruction.Rm]);
+  uint32_t  Rs     = to_int(reg[instruction.Rs]);
+  uint32_t  Rn     = to_int(reg[instruction.Rn]);
+  uint32_t  result = Rm * Rs;
 
   // the accumulate bit is set
   if (instruction.acc)
-    {
-      result += Rn;
-    }
+  {
+    result += Rn;
+  }
 
   // Save the result
-  reg[instruction.Rd] = to_bf (result);
+  reg[instruction.Rd] = to_bf(result);
 
   // If the set_cond bit is set, we need to update the CPSR
   if (instruction.set_cond)
-    {
-      arm_state->neg = get_bit (result, 31);
-      arm_state->zero = result == 0;
-    }
+  {
+    arm_state->neg  = get_bit(result, 31);
+    arm_state->zero = result == 0;
+  }
 }
 
-void execute_BRANCH (branch_t instruction, ArmState arm_state)
+void execute_BRANCH(branch_t instruction, ArmState arm_state)
 {
-  uint32_t offset = to_int (arm_state->reg[instruction.offset]);
-  int sign_bit = get_bit (offset, 24);
-  uint32_t mask = 0;
+  uint32_t offset   = to_int(arm_state->reg[instruction.offset]);
+  int      sign_bit = get_bit(offset, 24);
+  uint32_t mask     = 0;
   uint32_t extended = 0;
 
   // The offset will take into account the effect of the pipeline (i.e. PC is 8
@@ -211,17 +183,17 @@ void execute_BRANCH (branch_t instruction, ArmState arm_state)
   offset <<= 2;
 
   if (sign_bit == 0)
-    {
-      mask = ~(2 ^ 25 + 2 ^ 26);
-      extended = offset & mask;
-    }
+  {
+    mask     = ~(2 ^ 25 + 2 ^ 26);
+    extended = offset & mask;
+  }
   else
+  {
+    for (int i = 31; i > 24; i--)
     {
-      for (int i = 31; i > 24; i--)
-        {
-          mask += 2 ^ i;
-        }
-      extended = offset | mask;
+      mask += 2 ^ i;
     }
+    extended = offset | mask;
+  }
   arm_state->pc += extended;
 }
