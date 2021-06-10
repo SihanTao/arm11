b5eee9f302bf2ebd3ba27e74a8c85b59deffbf6f
Sihan Tao <sihan.tao20@imperial.ac.uk>
2021-06-10 14:44:58 -0700

    Implement is_label_line

--- src/assembler/parse/parse_helper.h
+++ src/assembler/parse/parse_helper.h
@@ -10,5 +10,5 @@ extern void preprocess_and_gen_sym_table(char **     loaded_file,
 extern void tokenize(char * line, TokenStream token_stream);
 extern bool is_label(char * line);
 extern NotSure allocate_address(TokenStream token_queue, SymbolTable symbol_table);
-
+extern bool is_label_line(char* line);
 #endif // PARSE_HELPER
