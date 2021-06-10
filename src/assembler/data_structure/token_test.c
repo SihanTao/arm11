065b190609ad348485401d3d2c991c7e507deb73
Sihan Tao <sihan.tao20@imperial.ac.uk>
2021-06-10 14:33:14 -0700

    add test for comma count

--- src/assembler/data_structure/token_test.c
+++ src/assembler/data_structure/token_test.c
@@ -1,6 +1,7 @@
 #include "token_stream.h"
 #include "../../emulator/utils/unit_test.h"
 #include <stdio.h>
+#include "../file_loader/assembler_file_loader.h"
 
 int main(void){
 	add_test("Test get opcode:");
@@ -12,6 +13,13 @@ int main(void){
 //		printf("%s\n", rest);
 		test_string_eq_v(token->opcode, "mov", "opcode == mov");
 		test_string_eq_v(rest, "r1,#3", "rest == r1,#3");
+	}
+
+	add_test("Test count-comma");
+	{
+		char instruction[] = "mov r1,#3";
+		char *rest = get_opcode(token, instruction);
+		test_int_v(comma_count(rest), 1, "comma == 1");
 	}
 	end_all_tests();
 }
