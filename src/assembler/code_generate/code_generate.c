1a0df3d26074f0a863bf1202876d7b011514c017
tony <tony_t_liu@outlook.com>
2021-06-11 22:47:12 +0800

    Tony and Tracy : function signature of decode

--- src/assembler/code_generate/code_generate.c
+++ src/assembler/code_generate/code_generate.c
@@ -1,13 +1,13 @@
 #include "../../global_utils/types_and_macros.h"
 
 #include "../global_utils/types_and_macros.h"
+#include "../global_utils/tools.h"
 
 bitfield encode_DP(proc_t instruction)
 {
   uint32_t result = 0;
-  set_bit_range(&result,instruction.cond, 28, 31);
+  set_bit_range(&result, instruction.cond, 28, 31);
   set_bit(&result, instruction.iFlag, 25);
   set_bit_range(&result, instruction.opcode, 21, 24);
   set_bit_range(&result, instruction.Rn, 16, 19);
-
 }
