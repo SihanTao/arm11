e71a7205d506ae7ab61cd9e5369f7498991641a7
tony <tony_t_liu@outlook.com>
2021-06-12 07:06:07 +0800

    Tony : added test for symbol table

---
+++ src/assembler/data_structure/symbol_table_test.c
@@ -0,0 +1,23 @@
+#include "symbol_table.h"
+
+int main(void)
+{
+  add_test("test_symbol_table");
+  {
+    SymbolTable symbol_table = init_symbol_table();
+    add_symbol_table("d", 1, symbol_table);
+    add_symbol_table("b", 2, symbol_table);
+    add_symbol_table("a", 3, symbol_table);
+    add_symbol_table("e", 4, symbol_table);
+
+    test_int(find_symbol_table("a", symbol_table), 3);
+    test_int(find_symbol_table("b", symbol_table), 2);
+    test_int(find_symbol_table("d", symbol_table), 1);
+    test_int(find_symbol_table("e", symbol_table), 4);
+
+    free_symbol_table(symbol_table);
+  }
+
+  end_all_tests();
+  return 0;
+}
