02889f378ea87fd072eae3a5a3485f281435d98d
tony <tony_t_liu@outlook.com>
2021-06-11 20:52:18 +0800

    declared function signatures and fixed fypes and macros

--- src/emulator/decode/decode.c
+++ src/emulator/decode/decode.c
@@ -21,22 +21,27 @@ void find_type(instruction_t *instruction)
   if (get_bit(n, 27))
   {
     instruction->tag = BRANCH;
+    // instruction->word.bf = convert_endian(instruction->word.bf);
   }
   else if (get_bit(n, 26))
   {
     instruction->tag = TRANS;
+    // instruction->word.bf = convert_endian(instruction->word.bf);
   }
   else if (get_bit(n, 25))
   {
     instruction->tag = DATA_PROCESS; // the case with op2 an immediate value
+    // instruction->word.bf = convert_endian(instruction->word.bf);
   }
   else if (get_bit(n, 4) && get_bit(n, 7))
   {
     instruction->tag = MUL;
+    // instruction->word.bf = convert_endian(instruction->word.bf);
   }
   else
   {
     instruction->tag = DATA_PROCESS;
+    instruction->word.bf = convert_endian(instruction->word.bf);
   }
 }
 
---
+++ src/global_utils/tools.h
@@ -0,0 +1,16 @@
+#ifndef GLOBAL_TOOLS
+#define GLOBAL_TOOLS
+
+
+// target = 0b011101001 start = 0 end = 3
+// returns : 0b1001
+int get_bit_range(int target,int start,int end);
+
+// dest = 101001000101 src = 10001 start = 0 end = 5
+// dest = 101001010001
+void set_bit_range(int* dest, int src, int start, int end);
+
+void set_bit(int* dest, int position, bool value);
+
+
+#endif //GLOBAL_TOOLS
