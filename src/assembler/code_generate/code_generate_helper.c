3efcc1d24b7299c5183962d5a2c1c1c3c7e7f33a
tony <tony_t_liu@outlook.com>
2021-06-12 06:35:30 +0800

    Tony : add before merge

--- src/assembler/code_generate/code_generate_helper.c
+++ src/assembler/code_generate/code_generate_helper.c
@@ -8,12 +8,12 @@
 
 uint32_t to_bcode_mov(Token cur_token)
 {
-  instruction_t intermidiate_rep;
+  proc_t intermidiate_rep;
 
   // retrive information from cur_token
   // initialize intermidiate_rep
   // call encode_DP
-  
+
   intermidiate_rep.tag = DATA_PROCESS;
   intermidiate_rep.word.proc.cond = ?;
 intermidiate_rep.word.proc.iFlag ;
