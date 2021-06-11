7013831db988fa6ddd596623ff2ddc5fb84883b7
李冉晨 <liranchen@liranchendeMacBook-Pro.local>
2021-06-11 21:38:58 +0800

    implement some bit operating function in tools

---
+++ src/global_utils/tools.c
@@ -0,0 +1,90 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "types_and_macros.h"
+#include "tools.h"
+
+void convert_endian_ptr(char *buffer)
+{
+  char temp;
+  temp      = buffer[0];
+  buffer[0] = buffer[3];
+  buffer[3] = temp;
+  temp      = buffer[1];
+  buffer[1] = buffer[2];
+  buffer[2] = temp;
+}
+
+bitfield convert_endian(bitfield word)
+{
+  byte temp;
+  temp       = word.byte1;
+  word.byte1 = word.byte4;
+  word.byte4 = temp;
+  temp       = word.byte2;
+  word.byte2 = word.byte3;
+  word.byte3 = temp;
+  return word;
+}
+
+void print_bit(uint32_t i)
+{
+  for (int k = INT_BIT_LENGTH - 1; k >= 0; k--)
+  {
+    printf("%d", get_bit(i, k));
+  }
+  printf("\n");
+}
+
+int get_bit(uint32_t i, int k) { return (i & (1 << k)) >> k; }
+
+// It can perform some conditional big-endian convertion before return
+uint32_t to_int(bitfield bf)
+{
+  if (TARGET_MACHINE_ENDIAN == BIG)
+  {
+    bf = convert_endian(bf);
+  }
+  uint32_t *result = (uint32_t *)&bf;
+  return *result;
+}
+
+// It can perform some conditional big-endian convertion before return
+bitfield to_bf(uint32_t i)
+{
+  bitfield *result = (bitfield *)&i;
+  if (TARGET_MACHINE_ENDIAN == BIG)
+  {
+    *result = convert_endian(*result);
+  }
+  return *result;
+}
+
+int get_bit_range(int target,int start,int end)
+{
+  int length = end - start + 1;
+  int mask = 0;
+  for (int i=0; i<length; i++) {
+    mask += pow(2, i);
+  }
+  return (target >> start) & mask;	
+}
+
+void set_bit(int* dest, bool value, int position) {
+  if (get_bit(*dest, position)) {
+    if (!value) {
+      *dest -= pow(2, position);
+    }
+  } else if (value) {
+    *dest += pow(2, position);
+  }
+}
+
+void set_bit_range(int* dest, int src, int start, int end)
+{
+  for (int i=start; i<=src; i++) {
+    set_bit(dest, get_bit(src, i), i);
+  }
+}
--- src/global_utils/tools.h
+++ src/global_utils/tools.h
@@ -1,7 +1,7 @@
 #ifndef GLOBAL_TOOLS
 #define GLOBAL_TOOLS
 
-
+#define INT_BIT_LENGTH 32
 // target = 0b011101001 start = 0 end = 3
 // returns : 0b1001
 int get_bit_range(int target,int start,int end);
@@ -11,6 +11,23 @@ int get_bit_range(int target,int start,int end);
 void set_bit_range(int* dest, int src, int start, int end);
 
 void set_bit(int* dest, bool value, int position);
+
+/*
+ * Print the bit representation
+ * of given uint32_t integer
+ */
+extern void print_bit (uint32_t i);
+
+/*
+ * Return the kth bit of a
+ * given integer
+ */
+extern int get_bit (uint32_t i, int k);
+
+extern void convert_endian_ptr (char *buffer);
+extern bitfield convert_endian (bitfield word);
+extern uint32_t to_int (bitfield b);
+extern bitfield to_bf (uint32_t i);
 
 
 #endif //GLOBAL_TOOLS
