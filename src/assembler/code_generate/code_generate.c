00f47919b78e16c807629b2e5e9ef5ff5a01395a
cl2420 <lcx@LAPTOP-QD7DAL3K.localdomain>
2021-06-11 22:35:03 +0800

    add encode in code generation

--- src/assembler/code_generate/code_generate.c
+++ src/assembler/code_generate/code_generate.c
@@ -8,6 +8,41 @@ bitfield encode_DP(proc_t instruction)
   set_bit_range(&result,instruction.cond, 28, 31);
   set_bit(&result, instruction.iFlag, 25);
   set_bit_range(&result, instruction.opcode, 21, 24);
+  set_bit(&result, instruction.set_cond, 20);
   set_bit_range(&result, instruction.Rn, 16, 19);
+  set_bit_range(&result, instruction.Rd, 12, 15);
+  set_bit_range(&result, instruction.operand2, 0, 11);
+}
+
+bitfield encode_MUL(mul_t instruction)
+{
+  uint32_t result = 0;
+  set_bit_range(&result,instruction.cond, 28, 31);
+  set_bit(&result, instruction.acc, 21);
+  set_bit(&result, instruction.set_cond, 20);
+  set_bit_range(&result, instruction.Rd, 16, 19);
+  set_bit_range(&result, instruction.Rn, 12, 15);
+  set_bit_range(&result, instruction.Rs, 8, 11);
+  set_bit_range(&result, instruction.Rm, 0, 3);
+}
+
+bitfield encode_TRANS(trans_t instruction)
+{
+  uint32_t result = 0;
+  set_bit_range(&result,instruction.cond, 28, 31);
+  set_bit(&result, instruction.iFlag, 25);
+  set_bit(&result, instruction.is_pre, 24);
+  set_bit(&result, instruction.is_up, 23);
+  set_bit(&result, instruction.is_load, 20);
+  set_bit_range(&result, instruction.Rn, 16, 19);
+  set_bit_range(&result, instruction.Rd, 12, 15);
+  set_bit_range(&result, instruction.offset, 0, 11);
+}
+
+bitfield encode_BRANCH(branch_t instruction)
+{
+  uint32_t result = 0;
+  set_bit_range(&result,instruction.cond, 28, 31);
+  set_bit_range(&result, instruction.offset, 0, 23);
 
 }
