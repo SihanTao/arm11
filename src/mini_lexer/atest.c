a249e0f9ad604ef1aee874ed2f76c27b1bcc6a03
tony <tony_t_liu@outlook.com>
2021-06-15 15:11:28 +0800

    Tony : implemented encode decode for mul

---
+++ src/mini_lexer/atest.c
@@ -0,0 +1,45 @@
+ Parsec abc = match("first", "abc");
+  Parsec cde = match("second", "cde");
+  Parsec ghf = match("third", "ghf");
+
+  Parsec and1 = make_and("and1", abc, cde);
+  Parsec and2 = make_and("and2", ghf, and1);
+
+  print_pc(and2, 0);
+  printf("\n");
+
+  char * string      = "ghfabccde";
+  char **char_stream = &string;
+  AST    ast         = parse(char_stream, and2, NULL);
+  print_ast(ast, 0);
+  printf("\n");
+
+  string          = "ghfabccde";
+  char_stream     = &string;
+  Parsec asdf     = match("asdfasdf", "asdf");
+  Parsec make_or1 = make_or("make_or", asdf, and2);
+  ast             = parse(char_stream, make_or1, NULL);
+  print_ast(ast, 0);
+  printf("\n");
+
+  string          = "ghfabccde";
+  char_stream     = &string;
+  Parsec make_or2 = make_or("make_or", and2, asdf);
+  ast             = parse(char_stream, make_or2, NULL);
+
+  print_ast(ast, 0);
+  printf("\n");
+
+  string      = "asdf";
+  char_stream = &string;
+  ast         = parse(char_stream, make_or2, NULL);
+
+  print_ast(ast, 0);
+  printf("\n");
+
+  Parsec and_components[3]
+      = { match("one", "one"), match("two", "two"), match("three", "three") };
+
+  Parsec my_and = seq("my add", and_components, 3);
+  print_pc(my_and, 0);
+  printf("\n");
--- src/mini_lexer/parserc_test.c
+++ src/mini_lexer/parserc_test.c
@@ -29,10 +29,6 @@ int encode_reg(AST ast)
   return atoi(reg_num);
 }
 
-// Parsec operand2()
-// {
-//   return make_or(hash_expr(), shifted_reg());
-// }
 
 int encode_mul(AST ast)
 {
@@ -46,6 +42,7 @@ int encode_mul(AST ast)
   {
     // some logic
   }
+  return 0;
 }
 
 Parsec p_mul()
@@ -58,55 +55,12 @@ Parsec p_mul()
                  seq("mla", sequence2, 5));
 }
 
-
-
 int main(void)
 {
-  Parsec abc = match("first", "abc");
-  Parsec cde = match("second", "cde");
-  Parsec ghf = match("third", "ghf");
-
-  Parsec and1 = make_and("and1", abc, cde);
-  Parsec and2 = make_and("and2", ghf, and1);
-
-  print_pc(and2, 0);
-  printf("\n");
-
-  char * string      = "ghfabccde";
-  char **char_stream = &string;
-  AST    ast         = parse(char_stream, and2, NULL);
-  print_ast(ast, 0);
-  printf("\n");
-
-  string          = "ghfabccde";
-  char_stream     = &string;
-  Parsec asdf     = match("asdfasdf", "asdf");
-  Parsec make_or1 = make_or("make_or", asdf, and2);
-  ast             = parse(char_stream, make_or1, NULL);
-  print_ast(ast, 0);
-  printf("\n");
-
-  string          = "ghfabccde";
-  char_stream     = &string;
-  Parsec make_or2 = make_or("make_or", and2, asdf);
-  ast             = parse(char_stream, make_or2, NULL);
-
-  print_ast(ast, 0);
-  printf("\n");
-
-  string      = "asdf";
-  char_stream = &string;
-  ast         = parse(char_stream, make_or2, NULL);
-
-  print_ast(ast, 0);
-  printf("\n");
-
-  Parsec and_components[3]
-      = { match("one", "one"), match("two", "two"), match("three", "three") };
-
-  Parsec my_and = seq("my add", and_components, 3);
-  print_pc(my_and, 0);
-  printf("\n");
+ char * string;
+ CharStream char_stream;
+ AST ast;
+ Parsec parsec;
 
   string      = "r12,";
   char_stream = &string;
@@ -127,9 +81,9 @@ int main(void)
   string      = "mla r12,r2,r3,r4\n";
   char_stream = &string;
 
-  abc = p_mul();
+  parsec = p_mul();
 
-  ast = parse(char_stream, abc, NULL);
+  ast = parse(char_stream, parsec, NULL);
 
   print_ast(ast, 0);
   printf("\n");
