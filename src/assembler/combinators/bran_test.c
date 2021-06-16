3b45eed58a2d10750b2ac9be75affa324f541c37
tony <tony_t_liu@outlook.com>
2021-06-17 00:57:58 +0800

    Tony : tested bran

--- src/assembler/combinators/bran_test.c
+++ src/assembler/combinators/bran_test.c
@@ -14,12 +14,12 @@
 
 int main(void)
 {
-  char *     string;
-  CharStream char_stream;
-  AST        ast;
-  Parsec     parsec;
+  char *        string;
+  CharStream    char_stream;
+  AST           ast;
+  Parsec        parsec;
   instruction_t bran_ins;
-  SymbolTable symbol_table = init_symbol_table();
+  SymbolTable   symbol_table = init_symbol_table();
   add_symbol_table("wait", 123, symbol_table);
 
   string      = "b wait";
@@ -28,29 +28,32 @@ int main(void)
   print_ast(ast, 0);
   printf("\n");
   bran_ins = e_bran(ast, symbol_table);
-  printf("bran_ins.tag :>> %d\n,", bran_ins.tag); //DELETE_MARK
-  printf("bran_ins.cond :>> %d\n,", bran_ins.cond); //DELETE_MARK
-  printf("bran_ins.word.branch.offset :>> %d\n,", bran_ins.word.branch.offset); //DELETE_MARK
+  printf("bran_ins.tag :>> %d\n", bran_ins.tag);   // DELETE_MARK
+  printf("bran_ins.cond :>> %d\n", bran_ins.cond); // DELETE_MARK
+  printf("bran_ins.word.branch.offset :>> %d\n",
+         bran_ins.word.branch.offset); // DELETE_MARK
 
   string      = "bal wait";
   char_stream = &string;
   ast         = parse(char_stream, p_bran(), NULL);
   print_ast(ast, 0);
   printf("\n");
-    bran_ins = e_bran(ast, symbol_table);
+  bran_ins = e_bran(ast, symbol_table);
 
-    printf("bran_ins.tag :>> %d\n,", bran_ins.tag); //DELETE_MARK
-  printf("bran_ins.cond :>> %d\n,", bran_ins.cond); //DELETE_MARK
-  printf("bran_ins.word.branch.offset :>> %d\n,", bran_ins.word.branch.offset); //DELETE_MARK
+  printf("bran_ins.tag :>> %d\n", bran_ins.tag);   // DELETE_MARK
+  printf("bran_ins.cond :>> %d\n", bran_ins.cond); // DELETE_MARK
+  printf("bran_ins.word.branch.offset :>> %d\n",
+         bran_ins.word.branch.offset); // DELETE_MARK
 
   string      = "beq wait";
   char_stream = &string;
   ast         = parse(char_stream, p_bran(), NULL);
   print_ast(ast, 0);
-    bran_ins = e_bran(ast, symbol_table);
+  bran_ins = e_bran(ast, symbol_table);
 
   printf("\n");
-    printf("bran_ins.tag :>> %d\n,", bran_ins.tag); //DELETE_MARK
-  printf("bran_ins.cond :>> %d\n,", bran_ins.cond); //DELETE_MARK
-  printf("bran_ins.word.branch.offset :>> %d\n,", bran_ins.word.branch.offset); //DELETE_MARK
+  printf("bran_ins.tag :>> %d\n", bran_ins.tag);   // DELETE_MARK
+  printf("bran_ins.cond :>> %d\n", bran_ins.cond); // DELETE_MARK
+  printf("bran_ins.word.branch.offset :>> %d\n",
+         bran_ins.word.branch.offset); // DELETE_MARK
 }
