f1968f667b78794f793afbc8718da9045b4a8636
tony <tony_t_liu@outlook.com>
2021-06-18 08:10:30 +0800

    Tony : cleaned the interface

--- src/assembler/combinators/bran.h
+++ src/assembler/combinators/bran.h
@@ -1,11 +1,10 @@
 #ifndef BRAN_COMBINATOR
 #define BRAN_COMBINATOR
 
+extern Parsec        p_bran_cond(void);
+extern Parsec        p_bran(void);
+extern cond_type     e_bran_cond(AST bran_cond);
+extern instruction_t e_bran(AST bran, SymbolTable symbol_table,
+                            int current_address);
 
-extern Parsec p_bran_cond(void);
-extern Parsec p_bran(void);
-
-cond_type e_bran_cond(AST bran_cond);
-instruction_t e_bran(AST bran, SymbolTable symbol_table, int current_address);
-
-#endif //BRAN_COMBINATOR
+#endif // BRAN_COMBINATOR
