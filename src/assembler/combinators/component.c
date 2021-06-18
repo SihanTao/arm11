70891d4f8f4a08e76ecf1cf29a1d33d942ec5c71
tony <tony_t_liu@outlook.com>
2021-06-18 13:15:25 +0800

    Tony : solved the bug of negative hexadecimal number

--- src/assembler/combinators/component.c
+++ src/assembler/combinators/component.c
@@ -66,7 +66,7 @@ bool is_space(char target)
 /*!
  * @param name
  * @return a parser combinator of number;
-*/
+ */
 Parsec p_number(char *name)
 {
   return take_while(name, isnumber);
@@ -74,7 +74,7 @@ Parsec p_number(char *name)
 
 /*!
  * @return an encoded decimal.
-*/
+ */
 int e_deci(AST number)
 {
   return atoi($T(number));
@@ -82,25 +82,35 @@ int e_deci(AST number)
 
 /*!
  * @return a parser combinator of Hexadecimal.
-*/
+ */
 Parsec p_hexa(char *name)
 {
-  return make_and(name, match(NULL, "0x"), take_while("hexa", isalnum));
+  return make_or(
+      name, make_and("pos", match(NULL, "0x"), take_while("hexa", isalnum)),
+      make_and("neg", match(NULL, "-0x"), take_while("hexa", isalnum)) );
 }
 
 /*!
  * @return an encoded Hexadecimal.
-*/
+ */
 int e_hexa(AST hexa)
 {
-  char *val = $TG(hexa, "hexa");
-  return strtol(val, NULL, 16);
+  AST pos = $G(hexa, "pos");
+  if (pos)
+  {
+    char * val = $TG(pos, "hexa");
+    return strtol(val, NULL, 16);
+  }
+
+  AST neg = $G(hexa, "neg");
+  char * val = $TG(neg, "hexa");
+  return -strtol(val, NULL, 16);
 }
 
 /*!
  * @return a parser combinator of hash expression.
-*/
-Parsec p_hash_expr(char * name)
+ */
+Parsec p_hash_expr(char *name)
 {
   return make_and(name, match(NULL, "#"),
                   make_or("number", p_hexa("hexa"), p_number("deci")));
@@ -108,8 +118,8 @@ Parsec p_hash_expr(char * name)
 
 /*!
  * @return a parser combinator of '=' expression
-*/
-Parsec p_eq_expr(char * name)
+ */
+Parsec p_eq_expr(char *name)
 {
   return make_and(name, match(NULL, "="),
                   make_or("number", p_hexa("hexa"), p_number("deci")));
@@ -117,7 +127,7 @@ Parsec p_eq_expr(char * name)
 
 /*!
  * @return an encoded '=' or hash expression
-*/
+ */
 int e_eq_hash_expr(AST hash_expr)
 {
   AST number = $G(hash_expr, "number");
@@ -130,9 +140,9 @@ int e_eq_hash_expr(AST hash_expr)
 
 Parsec p_reg_i(char *name)
 {
-  Parsec alts[3] = {match(NULL, ", "), match(NULL, ","), match(NULL, ",  ")};
-  Parsec seq1[3] = { match(NULL, "r"), p_number("reg_num"),
-                     alt(NULL, alts, 3) };
+  Parsec alts[3] = { match(NULL, ", "), match(NULL, ","), match(NULL, ",  ") };
+  Parsec seq1[3]
+      = { match(NULL, "r"), p_number("reg_num"), alt(NULL, alts, 3) };
   return seq(name, seq1, 3);
 }
 
@@ -143,7 +153,7 @@ Parsec p_reg_e(char *name)
 
 /*!
  * @return an encoded register.
-*/
+ */
 int e_reg(AST reg)
 {
   return atoi($TG(reg, "reg_num"));
@@ -151,7 +161,7 @@ int e_reg(AST reg)
 
 /*!
  * @return a parser combinator of operand2.
-*/
+ */
 Parsec p_operand2(void)
 {
   return make_or("operand2", p_hash_expr("imm val"), p_reg_e("Rm"));
@@ -159,8 +169,8 @@ Parsec p_operand2(void)
 
 /*!
  * @return an encoded operand2.
-*/
-reg_or_imm_t e_operand2(AST operand2, bool* is_imm)
+ */
+reg_or_imm_t e_operand2(AST operand2, bool *is_imm)
 {
   reg_or_imm_t result;
   AST          hash_ast = $G(operand2, "imm val");
@@ -190,7 +200,7 @@ reg_or_imm_t e_operand2(AST operand2, bool* is_imm)
 
 /*!
  * @return a parser combinator of no offset.
-*/
+ */
 Parsec p_no_offset(void)
 {
   Parsec seqs[3] = { match(NULL, "["), p_reg_e("Rn"), match(NULL, "]") };
@@ -199,7 +209,7 @@ Parsec p_no_offset(void)
 
 /*!
  * @return an encoded no offset.
-*/
+ */
 address_t e_no_offset(AST no_offset)
 {
   address_t result;
@@ -212,7 +222,7 @@ address_t e_no_offset(AST no_offset)
 
 /*!
  * @return a parser combinator of has offset.
-*/
+ */
 Parsec p_has_offset(void)
 {
   Parsec seqs[4] = { match(NULL, "["), p_reg_i("Rn"), p_hash_expr("offset"),
@@ -222,7 +232,7 @@ Parsec p_has_offset(void)
 
 /*!
  * @return an encoded has offset.
-*/
+ */
 address_t e_has_offset(AST has_offset)
 {
   address_t result;
@@ -235,7 +245,7 @@ address_t e_has_offset(AST has_offset)
 
 /*!
  * @return a parser combinator of pre index.
-*/
+ */
 Parsec p_pre_index(void)
 {
   return make_or("pre index", p_no_offset(), p_has_offset());
@@ -243,7 +253,7 @@ Parsec p_pre_index(void)
 
 /*!
  * @return an encoded pre insex.
-*/
+ */
 address_t e_pre_index(AST pre_index)
 {
   AST no_offset = $G(pre_index, "no offset");
@@ -261,7 +271,7 @@ address_t e_pre_index(AST pre_index)
 
 /*!
  * @return a parser combinator of pos index.
-*/
+ */
 Parsec p_post_index(void)
 {
   Parsec seqs[3] = { p_no_offset(), match(NULL, ","), p_hash_expr("offset") };
@@ -270,7 +280,7 @@ Parsec p_post_index(void)
 
 /*!
  * @return an encode pos index.
-*/
+ */
 address_t e_post_index(AST post_index)
 {
   address_t result         = e_no_offset($G(post_index, "no offset"));
@@ -282,7 +292,7 @@ address_t e_post_index(AST post_index)
 
 /*!
  * @return a parser combinator of address.
-*/
+ */
 Parsec p_address(void)
 {
   Parsec alts[3] = { p_eq_expr("eq expr"), p_post_index(), p_pre_index() };
@@ -291,7 +301,7 @@ Parsec p_address(void)
 
 /*!
  * @return an encoded '=' expression.
-*/
+ */
 address_t e_eq_expr(AST imm)
 {
   address_t result;
@@ -304,7 +314,7 @@ address_t e_eq_expr(AST imm)
 
 /*!
  * @return an encoded address.
-*/
+ */
 address_t e_address(AST address)
 {
   AST imm = $G(address, "eq expr");
