9b251b13739a172f5f5c6963b1b1df728d7601b6
tony <tony_t_liu@outlook.com>
2021-06-18 19:36:34 +0800

    Tony : fixed tests

--- src/assembler/combinators/component_test.c
+++ src/assembler/combinators/component_test.c
@@ -18,34 +18,30 @@ int main(void)
 
   string      = "#10";
   char_stream = &string;
-
-  ast = perform_parse(char_stream, p_hash_expr("hash_expr"), NULL);
-
+  ast         = perform_parse(char_stream, p_hash_expr("hash_expr"), NULL);
   print_ast(ast, 0);
   printf("\n");
-  printf("e_hash_expr(ast) :>> %d\n", e_eq_hash_expr(ast)); // DELETE_MARK
+  printf("e_hash_expr(ast) :>> %d\n", e_eq_hash_expr(ast));
 
   string      = "#-0x10";
   char_stream = &string;
-  ast         = perform_parse(char_stream,
-                 p_hash_expr("hash_expr"), NULL);
+  ast         = perform_parse(char_stream, p_hash_expr("hash_expr"), NULL);
   print_ast(ast, 0);
-  printf("e_hash_expr(ast) :>> %d\n",
-           e_eq_hash_expr(ast));
+  printf("e_hash_expr(ast) :>> %d\n", e_eq_hash_expr(ast));
 
   string      = "=0x10";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_eq_expr("eq_expr"), NULL);
   print_ast(ast, 0);
   printf("\n");
-  printf("e_hash_expr(ast) :>> %d\n", e_eq_hash_expr(ast)); // DELETE_MARK
+  printf("e_hash_expr(ast) :>> %d\n", e_eq_hash_expr(ast));
 
   string      = "r10";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_reg_e("Rm"), NULL);
   print_ast(ast, 0);
   printf("\n");
-  printf("e_reg(ast) :>> %d\n", e_reg(ast)); // DELETE_MARK
+  printf("e_reg(ast) :>> %d\n", e_reg(ast));
 
   string      = "r10";
   char_stream = &string;
@@ -53,79 +49,91 @@ int main(void)
   print_ast(ast, 0);
   printf("\n");
   bool         imm;
-  reg_or_imm_t reg_or_imm1 = e_operand2(ast, &imm);
-  printf("imm :>> %d\n", imm);                                  // DELETE_MARK
-  printf("reg_or_imm1.val :>> %d\n", reg_or_imm1.shift_reg.Rm); // DELETE_MARK
+  bool         is_up;
+  reg_or_imm_t reg_or_imm1 = e_operand2(ast, &imm, &is_up);
+  printf("imm :>> %d\n", imm);
+  printf("reg_or_imm1.val :>> %d\n", reg_or_imm1.shift_reg.Rm);
 
   string      = "#14";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_operand2(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  reg_or_imm1 = e_operand2(ast, &imm);
-  printf("imm :>> %d\n", imm);                                 // DELETE_MARK
-  printf("reg_or_imm1.val :>> %d\n", reg_or_imm1.rot_imm.imm); // DELETE_MARK
+  bool imm;
+  bool is_up;
+  reg_or_imm1 = e_operand2(ast, &imm, &is_up);
+  printf("imm :>> %d\n", imm);
+  printf("reg_or_imm1.val :>> %d\n", reg_or_imm1.rot_imm.imm);
 
   string      = "[r10]";
   char_stream = &string;
-  ast         = perform_parse(char_stream, p_no_offset(), NULL);
+  ast         = perform_parse(char_stream, p_address(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  address_t address = e_no_offset(ast);
-  printf("address.Rn :>> %d\n", address.Rn);      // DELETE_MARK
-  printf("address.offset :>> %d\n", address.offset_or_eq_expr); // DELETE_MARK
-  printf("address.is_post :>> %d\n", address.is_post);      // DELETE_MARK
+  bool      imm;
+  bool      is_up;
+  address_t address = e_address(ast, &imm, &is_up);
+  printf("address.Rn :>> %d\n", address.Rn);
+  printf("address.offset :>> %d\n", address.eq_expr_val);
+  printf("address.is_post :>> %d\n", address.is_post);
 
   string      = "[r10]";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_pre_index(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  address = e_pre_index(ast);
-  printf("address.Rn :>> %d\n", address.Rn);      // DELETE_MARK
-  printf("address.offset :>> %d\n", address.offset_or_eq_expr); // DELETE_MARK
-  printf("address.is_post :>> %d\n", address.is_post);      // DELETE_MARK
+  bool imm;
+  bool is_up;
+  address = e_address(ast, &imm, &is_up);
+  printf("address.Rn :>> %d\n", address.Rn);
+  printf("address.offset :>> %d\n", address.is_eq_expr);
+  printf("address.is_post :>> %d\n", address.is_post);
 
   string      = "[r10,#10]";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_pre_index(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  address = e_pre_index(ast);
-  printf("address.Rn :>> %d\n", address.Rn);      // DELETE_MARK
-  printf("address.offset :>> %d\n", address.offset_or_eq_expr); // DELETE_MARK
-  printf("address.is_post :>> %d\n", address.is_post);      // DELETE_MARK
+  bool imm;
+  bool is_up;
+  address = e_address(ast, &imm, &is_up);
+  printf("address.Rn :>> %d\n", address.Rn);
+  printf("address.offset :>> %d\n", address.is_eq_expr);
+  printf("address.is_post :>> %d\n", address.is_post);
 
   string      = "[r10],#13";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_post_index(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  address = e_post_index(ast);
-  printf("address.Rn :>> %d\n", address.Rn);      // DELETE_MARK
-  printf("address.offset :>> %d\n", address.offset_or_eq_expr); // DELETE_MARK
-  printf("address.is_post :>> %d\n", address.is_post);      // DELETE_MARK
+  bool imm;
+  bool is_up;
+  address = e_address(ast, &imm, &is_up);
+  printf("address.Rn :>> %d\n", address.Rn);
+  printf("address.offset :>> %d\n", address.is_eq_expr);
+  printf("address.is_post :>> %d\n", address.is_post);
 
   string      = "[r10],#12";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_address(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  address = e_address(ast);
-  printf("address.Rn :>> %d\n", address.Rn);      // DELETE_MARK
-  printf("address.offset :>> %d\n", address.offset_or_eq_expr); // DELETE_MARK
-  printf("address.is_post :>> %d\n", address.is_post);      // DELETE_MARK
+  bool imm;
+  bool is_up;
+  address = e_address(ast, &imm, &is_up);
+  printf("address.Rn :>> %d\n", address.Rn);
+  printf("address.offset :>> %d\n", address.is_eq_expr);
+  printf("address.is_post :>> %d\n", address.is_post);
 
   string      = "=0x100";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_address(), NULL);
   print_ast(ast, 0);
   printf("\n");
-  address = e_address(ast);
-  printf("address.is_imm :>> %d\n", address.is_eq_expr);        // DELETE_MARK
-  printf("address.Rn :>> %d\n", address.Rn);      // DELETE_MARK
-  printf("address.offset :>> %d\n", address.offset_or_eq_expr); // DELETE_MARK
-  printf("address.is_post :>> %d\n", address.is_post);      // DELETE_MARK
+  bool imm;
+  bool is_up;
+  address = e_address(ast, &imm, &is_up);
+  printf("address.Rn :>> %d\n", address.Rn);
+  printf("address.offset :>> %d\n", address.is_eq_expr);
+  printf("address.is_post :>> %d\n", address.is_post);
 }
-
-
--- src/assembler/combinators/trans_test.c
+++ src/assembler/combinators/trans_test.c
@@ -28,67 +28,65 @@ int main(void)
   ast         = perform_parse(char_stream, p_trans(), NULL);
   print_ast(ast, 0);
   ins = e_trans(ast, 0, token_stream, 0);
-  printf("ins.cond :>> %d\n", ins.cond); // DELETE_MARK
-  printf("ins.tag :>> %d\n", ins.tag);   // DELETE_MARK
+  printf("ins.cond :>> %d\n", ins.cond);
+  printf("ins.tag :>> %d\n", ins.tag);
   trans = ins.word.trans;
-  printf("trans.is_load :>> %d\n", trans.is_load); // DELETE_MARK
-  printf("trans.is_pre :>> %d\n", trans.is_pre);   // DELETE_MARK
-  printf("trans.is_reg :>> %d\n", trans.is_reg);   // DELETE_MARK
-  printf("trans.is_up :>> %d\n", trans.is_up);     // DELETE_MARK
-  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm); // DELETE_MARK
-  printf("trans.Rd :>> %d\n", trans.Rd);                     // DELETE_MARK
-  printf("trans.Rn :>> %d\n", trans.Rn);                     // DELETE_MARK
+  printf("trans.is_load :>> %d\n", trans.is_load);
+  printf("trans.is_pre :>> %d\n", trans.is_pre);
+  printf("trans.is_reg :>> %d\n", trans.is_reg);
+  printf("trans.is_up :>> %d\n", trans.is_up);
+  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm);
+  printf("trans.Rd :>> %d\n", trans.Rd);
+  printf("trans.Rn :>> %d\n", trans.Rn);
 
   string      = "str r1,[r2]";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_trans(), NULL);
   print_ast(ast, 0);
   ins = e_trans(ast, 0, token_stream, 0);
-  printf("ins.cond :>> %d\n", ins.cond); // DELETE_MARK
-  printf("ins.tag :>> %d\n", ins.tag);   // DELETE_MARK
+  printf("ins.cond :>> %d\n", ins.cond);
+  printf("ins.tag :>> %d\n", ins.tag);
   trans = ins.word.trans;
-  printf("trans.is_load :>> %d\n", trans.is_load); // DELETE_MARK
-  printf("trans.is_pre :>> %d\n", trans.is_pre);   // DELETE_MARK
-  printf("trans.is_reg :>> %d\n", trans.is_reg);   // DELETE_MARK
-  printf("trans.is_up :>> %d\n", trans.is_up);     // DELETE_MARK
-  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm); // DELETE_MARK
-  printf("trans.Rd :>> %d\n", trans.Rd);                     // DELETE_MARK
-  printf("trans.Rn :>> %d\n", trans.Rn);                     // DELETE_MARK
+  printf("trans.is_load :>> %d\n", trans.is_load);
+  printf("trans.is_pre :>> %d\n", trans.is_pre);
+  printf("trans.is_reg :>> %d\n", trans.is_reg);
+  printf("trans.is_up :>> %d\n", trans.is_up);
+  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm);
+  printf("trans.Rd :>> %d\n", trans.Rd);
+  printf("trans.Rn :>> %d\n", trans.Rn);
 
   string      = "ldr r1,[r2,#100]";
   char_stream = &string;
   ast         = perform_parse(char_stream, p_trans(), NULL);
   print_ast(ast, 0);
   ins = e_trans(ast, 0, token_stream, 0);
-  printf("ins.cond :>> %d\n", ins.cond); // DELETE_MARK
-  printf("ins.tag :>> %d\n", ins.tag);   // DELETE_MARK
+  printf("ins.cond :>> %d\n", ins.cond);
+  printf("ins.tag :>> %d\n", ins.tag);
   trans = ins.word.trans;
-  printf("trans.is_load :>> %d\n", trans.is_load);           // DELETE_MARK
-  printf("trans.is_pre :>> %d\n", trans.is_pre);             // DELETE_MARK
-  printf("trans.is_reg :>> %d\n", trans.is_reg);             // DELETE_MARK
-  printf("trans.is_up :>> %d\n", trans.is_up);               // DELETE_MARK
-  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm); // DELETE_MARK
-  printf("trans.Rd :>> %d\n", trans.Rd);                     // DELETE_MARK
-  printf("trans.Rn :>> %d\n", trans.Rn);                     // DELETE_MARK
+  printf("trans.is_load :>> %d\n", trans.is_load);
+  printf("trans.is_pre :>> %d\n", trans.is_pre);
+  printf("trans.is_reg :>> %d\n", trans.is_reg);
+  printf("trans.is_up :>> %d\n", trans.is_up);
+  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm);
+  printf("trans.Rd :>> %d\n", trans.Rd);
+  printf("trans.Rn :>> %d\n", trans.Rn);
 
-  string      = "ldr r1,=0x100";
-  char_stream = &string;
-  ast         = perform_parse(char_stream, p_trans(), NULL);
+  string          = "ldr r1,=0x100";
+  char_stream     = &string;
+  ast             = perform_parse(char_stream, p_trans(), NULL);
   int end_address = 100;
   print_ast(ast, 0);
   ins = e_trans(ast, 0, token_stream, &end_address);
-  printf("ins.cond :>> %d\n", ins.cond); // DELETE_MARK
-  printf("ins.tag :>> %d\n", ins.tag);   // DELETE_MARK
+  printf("ins.cond :>> %d\n", ins.cond);
+  printf("ins.tag :>> %d\n", ins.tag);
   trans = ins.word.trans;
-  printf("trans.is_load :>> %d\n", trans.is_load);           // DELETE_MARK
-  printf("trans.is_pre :>> %d\n", trans.is_pre);             // DELETE_MARK
-  printf("trans.is_reg :>> %d\n", trans.is_reg);             // DELETE_MARK
-  printf("trans.is_up :>> %d\n", trans.is_up);               // DELETE_MARK
-  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm); // DELETE_MARK
-  printf("trans.Rd :>> %d\n", trans.Rd);                     // DELETE_MARK
-  printf("trans.Rn :>> %d\n", trans.Rn);                     // DELETE_MARK
-  printf("token_stream->tail->imm_val :>> %d\n",
-         token_stream->tail->imm_val); // DELETE_MARK
-  printf("end_address :>> %d\n", end_address); //DELETE_MARK
-
+  printf("trans.is_load :>> %d\n", trans.is_load);
+  printf("trans.is_pre :>> %d\n", trans.is_pre);
+  printf("trans.is_reg :>> %d\n", trans.is_reg);
+  printf("trans.is_up :>> %d\n", trans.is_up);
+  printf("trans.offset :>> %d\n", trans.offset.rot_imm.imm);
+  printf("trans.Rd :>> %d\n", trans.Rd);
+  printf("trans.Rn :>> %d\n", trans.Rn);
+  printf("token_stream->tail->imm_val :>> %d\n", token_stream->tail->imm_val);
+  printf("end_address :>> %d\n", end_address);
 }
